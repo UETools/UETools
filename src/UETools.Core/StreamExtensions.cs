06135686edbf649f4a97f3aea9720c4e562adfff
jesterret <jesterret@users.noreply.github.com>
2020-10-01 03:34:26 +0200

    Added missing netstandard2.0 changes

--- src/UETools.Core/StreamExtensions.cs
+++ src/UETools.Core/StreamExtensions.cs
@@ -1,5 +1,7 @@
 using System;
+using System.Buffers;
 using System.IO;
+using System.Runtime.InteropServices;
 using System.Threading;
 using System.Threading.Tasks;
 
@@ -9,7 +11,7 @@ namespace UETools.Core
     {
         public static void ReadWholeBuf(this Stream stream, Span<byte> buf)
         {
-            int remaining = buf.Length;
+            var remaining = buf.Length;
             var offset = 0;
             while (remaining > 0)
             {
@@ -49,7 +51,7 @@ namespace UETools.Core
 
         public static async ValueTask ReadWholeBufAsync(this Stream stream, Memory<byte> buf, CancellationToken cancellationToken = default)
         {
-            int remaining = buf.Length;
+            var remaining = buf.Length;
             var offset = 0;
             while (remaining > 0)
             {
@@ -86,5 +88,129 @@ namespace UETools.Core
                 offset += read;
             }
         }
+
+        // TODO: Copyrights
+        public static int Read(this Stream stream, Memory<byte> buffer)
+        {
+            if (MemoryMarshal.TryGetArray<byte>(buffer, out var array))
+            {
+                return stream.Read(array.Array!, array.Offset, array.Count);
+            }
+            else
+            {
+                var sharedBuffer = ArrayPool<byte>.Shared.Rent(buffer.Length);
+                try
+                {
+                    var result = stream.Read(sharedBuffer, 0, buffer.Length);
+                    new Span<byte>(sharedBuffer, 0, result).CopyTo(buffer.Span);
+                    return result;
+                }
+                finally
+                {
+                    ArrayPool<byte>.Shared.Return(sharedBuffer);
+                }
+            }
+        }
+        public static void Write(this Stream stream, ReadOnlyMemory<byte> buffer)
+        {
+            if (MemoryMarshal.TryGetArray(buffer, out var array))
+            {
+                stream.Write(array.Array!, array.Offset, array.Count);
+            }
+            else
+            {
+                var sharedBuffer = ArrayPool<byte>.Shared.Rent(buffer.Length);
+                try
+                {
+                    buffer.Span.CopyTo(sharedBuffer);
+                    stream.Write(sharedBuffer, 0, buffer.Length);
+                }
+                finally
+                {
+                    ArrayPool<byte>.Shared.Return(sharedBuffer);
+                }
+            }
+        }
+
+#if NETSTANDARD2_0
+        public static int Read(this Stream stream, Span<byte> buffer)
+        {
+            var sharedBuffer = ArrayPool<byte>.Shared.Rent(buffer.Length);
+            try
+            {
+                var result = stream.Read(sharedBuffer, 0, buffer.Length);
+                new Span<byte>(sharedBuffer, 0, result).CopyTo(buffer);
+                return result;
+            }
+            finally
+            {
+                ArrayPool<byte>.Shared.Return(sharedBuffer);
+            }
+        }
+        public static void Write(this Stream stream, ReadOnlySpan<byte> buffer)
+        {
+            var sharedBuffer = ArrayPool<byte>.Shared.Rent(buffer.Length);
+            try
+            {
+                buffer.CopyTo(sharedBuffer);
+                stream.Write(sharedBuffer, 0, buffer.Length);
+            }
+            finally
+            {
+                ArrayPool<byte>.Shared.Return(sharedBuffer);
+            }
+        }
+
+        public static ValueTask<int> ReadAsync(this Stream stream, Memory<byte> buffer, CancellationToken cancellationToken = default)
+        {
+            if (MemoryMarshal.TryGetArray<byte>(buffer, out var array))
+            {
+                return new ValueTask<int>(stream.ReadAsync(array.Array, array.Offset, array.Count, cancellationToken));
+            }
+            else
+            {
+                var sharedBuffer = ArrayPool<byte>.Shared.Rent(buffer.Length);
+                return FinishReadAsync(stream.ReadAsync(sharedBuffer, 0, buffer.Length, cancellationToken), sharedBuffer, buffer);
+
+                async ValueTask<int> FinishReadAsync(Task<int> readTask, byte[] localBuffer, Memory<byte> localDestination)
+                {
+                    try
+                    {
+                        var result = await readTask.ConfigureAwait(false);
+                        new Span<byte>(localBuffer, 0, result).CopyTo(localDestination.Span);
+                        return result;
+                    }
+                    finally
+                    {
+                        ArrayPool<byte>.Shared.Return(localBuffer);
+                    }
+                }
+            }
+        }
+        public static ValueTask WriteAsync(this Stream stream, ReadOnlyMemory<byte> buffer, CancellationToken cancellationToken = default)
+        {
+            if (MemoryMarshal.TryGetArray(buffer, out var array))
+            {
+                return new ValueTask(stream.WriteAsync(array.Array, array.Offset, array.Count, cancellationToken));
+            }
+            else
+            {
+                var sharedBuffer = ArrayPool<byte>.Shared.Rent(buffer.Length);
+                buffer.Span.CopyTo(sharedBuffer);
+                return new ValueTask(FinishWriteAsync(stream.WriteAsync(sharedBuffer, 0, buffer.Length, cancellationToken), sharedBuffer));
+            }
+        }
+        private static async Task FinishWriteAsync(Task writeTask, byte[] localBuffer)
+        {
+            try
+            {
+                await writeTask.ConfigureAwait(false);
+            }
+            finally
+            {
+                ArrayPool<byte>.Shared.Return(localBuffer);
+            }
+        }
+#endif
     }
 }
